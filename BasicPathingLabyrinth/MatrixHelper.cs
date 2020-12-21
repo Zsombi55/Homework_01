c782f00d15728873f133e8ea0227a3cf65f7f3ee
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2020-12-21 13:46:42 +0200

    A12, wip.

---
+++ BasicPathingLabyrinth/MatrixHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace BasicPathingLabyrinth
+{
+	class MatrixHelper
+	{
+		public static int[,] ReadMap()
+        {
+			String input = File.ReadAllText(@"../MapData.txt");
+
+			StreamReader readFile = new StreamReader(@"../MapData.txt");
+			if(int.TryParse(readFile.ReadLine(), out int size)) throw new Exception($"ERROR..  invalid or no value.");
+			string[] lengthCheck = readFile.ReadLine().Split(" ");
+			if(lengthCheck.Length != size) throw new Exception($"ERROR..  the specified size does not match the map length.");
+			
+			int rowi = 1, coli = 0;
+			int[,] result = new int[size, size];
+			foreach (var row in input.Split("\n"))
+			{
+				coli = 0;
+				foreach (var col in row.Trim().Split(" "))
+				{
+					result[rowi, coli] = int.Parse(col.Trim());
+					coli++;
+				}
+				rowi++;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ BasicPathingLabyrinth/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * User: Zsombor
+ * Date: 2020-12-21
+ * Time: 12:12
+ * 12th.
+ */
+
+using System;
+
+namespace BasicPathingLabyrinth
+{
+// Goal: square map data from txt file into matrix (incl. start point), print the path coordinates to the exit.
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			foreach(var line in MatrixHelper.ReadMap()) Console.WriteLine(string.Join(" ", line));
+
+			Console.WriteLine("Hello World!");
+		}
+
+
+	}
+}
